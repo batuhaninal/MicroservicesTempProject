2355ba1bab266fde741eafbbde69f7b05ebc3597
Batuhan Inal <np_batu@hotmail.com>
2023-09-25 22:34:10 +0300

    Jwt configuration and oauth 2 authorization

---
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using FreeCourse.IdentityServer.Dtos;
+using FreeCourse.IdentityServer.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace FreeCourse.IdentityServer.Controllers
+{
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> SignUp(SignupDto signupDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signupDto.UserName,
+                Email = signupDto.Email,
+                City = signupDto.City
+            };
+
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+
+            if(!result.Succeeded)
+            {
+                return BadRequest(Shared.Dtos.Response<Shared.Dtos.NoContent>.Fail(result.Errors.Select(x=>x.Description).ToList(), 400));
+            }
+
+            return NoContent();
+
+        }
+    }
+}
--- IdentityServer/FreeCourse.IdentityServer/Program.cs
+++ IdentityServer/FreeCourse.IdentityServer/Program.cs
@@ -2,7 +2,11 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using FreeCourse.IdentityServer.Data;
+using FreeCourse.IdentityServer.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -36,23 +40,21 @@ namespace FreeCourse.IdentityServer
                 .CreateLogger();
 
             try
-            {
-                var seed = args.Contains("/seed");
-                if (seed)
-                {
-                    args = args.Except(new[] { "/seed" }).ToArray();
-                }
-
+            {            
                 var host = CreateHostBuilder(args).Build();
 
-                if (seed)
+                using (var scope = host.Services.CreateScope())
                 {
-                    Log.Information("Seeding database...");
-                    var config = host.Services.GetRequiredService<IConfiguration>();
-                    var connectionString = config.GetConnectionString("DefaultConnection");
-                    SeedData.EnsureSeedData(connectionString);
-                    Log.Information("Done seeding database.");
-                    return 0;
+                    var serviceProvider = scope.ServiceProvider;
+
+                    var appDbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
+
+                    appDbContext.Database.Migrate();
+
+                    var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+                    if (!appDbContext.Users.Any())
+                        userManager.CreateAsync(new ApplicationUser { UserName = "admin", Email = "admin@admin.com", City = "Konya" }, "Admin12*").Wait();
                 }
 
                 Log.Information("Starting host...");
