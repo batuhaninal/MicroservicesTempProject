8be036deae9c7011429d6310f7b4d0048157623c
Batuhan Inal <np_batu@hotmail.com>
2023-10-29 01:53:48 +0300

    Token exchange for disocunt and fake payment services

--- Gateways/FreeCourse.Gateway/Program.cs
+++ Gateways/FreeCourse.Gateway/Program.cs
@@ -2,11 +2,14 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using System.IdentityModel.Tokens.Jwt;
+using FreeCourse.Gateway.DelegateHandlers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // sub degerini otomatik olarak nameidentifier claime mapleme ozelligini devre disi biraktik
 JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+builder.Services.AddHttpClient<TokenExchangeDelegateHandler>();
 
 builder.Services.AddAuthentication().AddJwtBearer("GatewayAuthenticationScheme",opt =>
 {
@@ -21,7 +24,7 @@ builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
     config.AddJsonFile($"configuration.{hostingContext.HostingEnvironment.EnvironmentName}.json").AddEnvironmentVariables(); 
 });
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot().AddDelegatingHandler<TokenExchangeDelegateHandler>();
 
 var app = builder.Build();
 
